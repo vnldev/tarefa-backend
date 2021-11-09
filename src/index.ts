33822c97c7c5ff5e00665e2ac5819a2a3de5dfd4
Victor Lima <victor-lima26@hotmail.com>
2021-11-08 23:14:11 -0300

    Adicionando variáveis de ambiente

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,35 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 import routes from "./routes";
 
+require("dotenv").config();
+
+const config:any = {
+   "type": process.env.DB_DIALECT,
+   "host": process.env.DB_HOST,
+   "port": process.env.DB_PORT,
+   "username": process.env.DB_USER,
+   "password": process.env.DB_PASSWORD,
+   "database": process.env.DB_NAME,
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      "./src/entity/*"
+   ],
+   "migrations": [
+      "src/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
+
 const app = express();
-createConnection();
+createConnection(config);
 
 app.use(cors());
 app.use(bodyParser.json());
